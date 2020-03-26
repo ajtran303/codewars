74cdd5e419d10bde6cc105c7477bba08c4817423
AJ Tran <anh.jamietran@gmail.com>
2020-03-26 13:05:27 -0600

    Add solutions

---
+++ ruby/two_to_one.rb
@@ -0,0 +1,12 @@
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+#
+# each taken only once - coming from s1 or s2.
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+
+def longest(a1, a2)
+  (a1+a2).split("").uniq.sort.join
+end
